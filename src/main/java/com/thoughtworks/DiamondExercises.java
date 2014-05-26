477232f027b379425e43de984b7599ed4ed58505
talkin <taojing1106@163.com>
2014-05-26 22:57:34 +0800

    add a function of Diamond

--- src/main/java/com/thoughtworks/DiamondExercises.java
+++ src/main/java/com/thoughtworks/DiamondExercises.java
@@ -30,5 +30,28 @@ public class DiamondExercises {
             }
             System.out.println("");
         }
+
+        System.out.println("Diamond");
+        System.out.println("Given a number n,print a centered diamond.");
+        System.out.print("n:");
+        int n2 = Integer.parseInt(scanner.next());
+        for (i=1; i<=n1; i++) {
+            for (int j=0; j<=n1-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j=1;j<=2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (i=2; i<=n1; i++) {
+            for (int j=1; j<=i; j++) {
+                System.out.print(" ");
+            }
+            for (int j=0; j<=2*(n1-i); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }

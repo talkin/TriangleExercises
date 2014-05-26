bebcc1c0509d57ee68fab59fcb066953f537c63a
talkin <taojing1106@163.com>
2014-05-26 17:53:29 +0800

    TriangleExercises : Easiest exercise ever

---
+++ src/main/java/com/thoughtworks/exercise.java
@@ -0,0 +1,14 @@
+package com.thoughtworks;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: talkin
+ * Date: 14-5-26
+ * Time: 下午5:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class exercise {
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+}

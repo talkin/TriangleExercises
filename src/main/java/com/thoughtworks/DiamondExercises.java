c6587f0b7d39742c3ca8fab0a55a37e8adbfecb3
talkin <taojing1106@163.com>
2014-05-27 00:01:54 +0800

    add a function of Diamond with Name

--- src/main/java/com/thoughtworks/DiamondExercises.java
+++ src/main/java/com/thoughtworks/DiamondExercises.java
@@ -14,7 +14,7 @@ public class DiamondExercises {
     public static void main(String args[]) {
         InputStream is = System.in;
         Scanner scanner = new Scanner(is);
-        int i;
+        int i,j;
 
         System.out.println("Diamond Exercises");
         System.out.println("Isosceles Triangle");
@@ -22,10 +22,10 @@ public class DiamondExercises {
         System.out.print("n:");
         int n1 = Integer.parseInt(scanner.next());
         for (i=1; i<=n1; i++) {
-            for (int j=0; j<n1-i; j++) {
+            for (j=0; j<n1-i; j++) {
                 System.out.print(" ");
             }
-            for (int j=1; j<=2*i-1; j++) {
+            for (j=1; j<=2*i-1; j++) {
                 System.out.print("*");
             }
             System.out.println("");
@@ -35,20 +35,46 @@ public class DiamondExercises {
         System.out.println("Given a number n,print a centered diamond.");
         System.out.print("n:");
         int n2 = Integer.parseInt(scanner.next());
-        for (i=1; i<=n1; i++) {
-            for (int j=0; j<=n1-i; j++) {
+        for (i=1; i<=n2; i++) {
+            for (j=0; j<=n2-i; j++) {
                 System.out.print(" ");
             }
-            for (int j=1;j<=2*i-1; j++) {
+            for (j=1;j<=2*i-1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for (i=2; i<=n1; i++) {
-            for (int j=1; j<=i; j++) {
+        for (i=2; i<=n2; i++) {
+            for (j=1; j<=i; j++) {
                 System.out.print(" ");
             }
-            for (int j=0; j<=2*(n1-i); j++) {
+            for (j=0; j<=2*(n2-i); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("Diamond with Name");
+        System.out.println("Given a number,print a centered diamond with your name in the place of middle line");
+        System.out.print("n:");
+        int n3 = Integer.parseInt(scanner.next());
+        for (i=1; i<n3; i++) {
+            for (j=0; j<=n3-i; j++) {
+                System.out.print(" ");
+            }
+            for (j=1; j<=2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        if (i==n3) {
+            System.out.print("taojing\n");
+        }
+        for (i=2; i<=n3; i++) {
+            for (j=1; j<=i; j++) {
+                System.out.print(" ");
+            }
+            for (j=0; j<=2*(n3-i); j++) {
                 System.out.print("*");
             }
             System.out.println("");
